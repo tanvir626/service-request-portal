d4d15f6419b3d7cab0c588012dc1ad313d61f6be
tanvir626 <testweb597@gmail.com>
2026-02-04 11:52:43 +0600

    setup_ASP.NET_Core_MVC_project_structure_#2

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace service_request_portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
